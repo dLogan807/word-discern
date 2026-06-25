60f9241136c4e1dd81dc1312b4af3576d31981eb
Dylan Logan <116318481+dLogan807@users.noreply.github.com>
2026-06-25 21:50:51 +1200

    fix LARGE_BARREL_MODULES message during build

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,18 @@ import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import path from "path";
+import transformImports from "@rolldown/plugin-transform-imports";
 
 export default defineConfig({
-  plugins: [babel({ presets: [reactCompilerPreset()] }), react()],
+  plugins: [
+    babel({ presets: [reactCompilerPreset()] }),
+    react(),
+    transformImports({
+      "@tabler/icons-react": {
+        transform: "@tabler/icons-react/dist/esm/icons/{{member}}",
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
