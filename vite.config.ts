aca2716ae625e9496adec5f40ed454e86c33e925
Dylan Logan <116318481+dLogan807@users.noreply.github.com>
2026-06-27 11:10:56 +1200

    remove @rolldown/plugin-transform-imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
-import transformImports from "@rolldown/plugin-transform-imports";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    transformImports({
-      "@tabler/icons-react": {
-        transform: "@tabler/icons-react/dist/esm/icons/{{member}}",
-      },
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     tsconfigPaths: true,
   },
