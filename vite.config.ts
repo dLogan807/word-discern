0e604b09825d0feb14c11c7ebaabf84caf5a1fa4
Dylan Logan <116318481+dLogan807@users.noreply.github.com>
2026-06-26 20:40:10 +1200

    replace manual src path handling

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,19 @@
-import { defineConfig } from "vite";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-import babel from "@rolldown/plugin-babel";
-import path from "path";
-import transformImports from "@rolldown/plugin-transform-imports";
+import babel from '@rolldown/plugin-babel';
+import transformImports from '@rolldown/plugin-transform-imports';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [
     babel({ presets: [reactCompilerPreset()] }),
     react(),
     transformImports({
-      "@tabler/icons-react": {
-        transform: "@tabler/icons-react/dist/esm/icons/{{member}}",
+      '@tabler/icons-react': {
+        transform: '@tabler/icons-react/dist/esm/icons/{{member}}',
       },
     }),
   ],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
+    tsconfigPaths: true,
   },
 });
