e2170187692673679f09729db9542ab992d6ffcd
Dylan Logan <116318481+dLogan807@users.noreply.github.com>
2026-06-27 11:05:36 +1200

    remove babel to fix failing android builds

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
-import babel from "@rolldown/plugin-babel";
 import transformImports from "@rolldown/plugin-transform-imports";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
-    babel({ presets: [reactCompilerPreset()] }),
     react(),
     transformImports({
       "@tabler/icons-react": {
