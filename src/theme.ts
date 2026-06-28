9ac58b062e0c8df7d7bcfc9a591b77b24e39c533
Dylan Logan <116318481+dLogan807@users.noreply.github.com>
2026-06-28 15:06:39 +1200

    change app theme to purple and change fonts

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,21 @@
 import { createTheme } from "@mantine/core";
 
 export const theme = createTheme({
-  /* Put your mantine theme override here */
+  primaryColor: "discerning-purple",
+  colors: {
+    "discerning-purple": [
+      "#faedff",
+      "#ecdaf7",
+      "#d3b3e9",
+      "#ba8ada",
+      "#a262cc",
+      "#9851c6",
+      "#9145c4",
+      "#7e37ad",
+      "#702f9b",
+      "#622689",
+    ],
+  },
+  fontFamily: "Arial, sans-serif",
+  headings: { fontFamily: "Times New Roman, sans-serif" },
 });
