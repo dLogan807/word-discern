9cf9d18397217db45c9f18fd93fccc623f9d992c
Dylan Logan <116318481+dLogan807@users.noreply.github.com>
2026-09-06 12:29:49 +1200

    implement csp

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,15 @@ export default defineConfig({
       "@": "/src",
     },
   },
+  server: {
+    headers: {
+      "Strict-Transport-Security": "max-age=31536000; includeSubDomains; preload",
+      "X-Content-Type-Options": "nosniff",
+      "X-Frame-Options": "DENY",
+      "X-XSS-Protection": "1; mode=block",
+      "Referrer-Policy": "strict-origin-when-cross-origin",
+      "Content-Security-Policy":
+        "default-src 'self'; script-src 'self' 'sha256-Eq6aw6yePf6E8rqkUS0hq7t/sMTD1xld4awQcUpNkfc='; connect-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self'; font-src 'self'; object-src 'none'; base-uri 'self'; form-action 'self'; frame-src 'self; frame-ancestors 'none'; upgrade-insecure-requests;",
+    },
+  },
 });
